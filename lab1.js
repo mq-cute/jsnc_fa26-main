be5add7f6335148cf6abd438085a12982487a558
mqcute <m.wuannn@gmail.com>
2026-09-15 15:54:02 +0700

    xong lab 1

---
+++ lab1.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+
+let name = "Nguyen Van A";
+const age = 20;
+const email = "a@gmail";
+
+console.log("Ten toi la: " + name);
+console.log("Tuoi: " + age);
+console.log("Email: " + email);
