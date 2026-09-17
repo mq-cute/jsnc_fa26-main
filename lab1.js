28dc829a67d7e9ec83c0aaeae4b905eec8dfb49b
mqcute <m.wuannn@gmail.com>
2026-09-17 15:28:20 +0700

    xong lab2,3,4

--- lab1.js
+++ lab1.js
@@ -1,5 +1,6 @@
 console.log("Hello, World!");
 
+// Bài 1 – Khai báo biến
 let name = "Nguyen Van A";
 const age = 20;
 const email = "a@gmail";
@@ -7,3 +8,32 @@ const email = "a@gmail";
 console.log("Ten toi la: " + name);
 console.log("Tuoi: " + age);
 console.log("Email: " + email);
+
+// Bài 2 – Thay đổi giá trị biến
+
+let age = 20;
+
+console.log(age);
+
+age = 21;
+
+console.log(age);
+
+
+const a = 10;
+const b = 5;
+
+console.log("Tổng:", a + b);
+console.log("Hiệu:", a - b);
+console.log("Tích:", a * b);
+console.log("Thương:", a / b);
+
+
+// Bài 4 – Hàm sayHello
+function sayHello(name) {
+    console.log("Xin chào " + name);
+}
+
+sayHello("An");
+sayHello("Minh");
+sayHello("Quân");
